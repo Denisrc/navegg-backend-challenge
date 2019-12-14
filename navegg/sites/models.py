e49da4d6f5793efd5338f26b585aac8fe4e72b6d
João Denis Rodrigues <joao.denisr@gmail.com>
2019-12-14 15:29:17 -0300

    create site models

--- navegg/sites/models.py
+++ navegg/sites/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Sites(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(null=False, max_length=100)
+    active = models.BooleanField(default=True)
+    url = models.CharField(null=False, max_length=100)
+    category = models.CharField(null=False, max_length=100)
