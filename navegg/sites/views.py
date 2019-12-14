8dca70a912001c9a4e933ab55c5a28d3b428876a
João Denis Rodrigues <joao.denisr@gmail.com>
2019-12-14 15:31:01 -0300

    create get list and post new element

--- navegg/sites/views.py
+++ navegg/sites/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from sites.models import Sites
+from sites.serializers import SitesSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class SitesList(APIView):
+    def get(self, request, format=None):
+        sites = Sites.objects.all()
+        serializer = SitesSerializer(sites, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SitesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
