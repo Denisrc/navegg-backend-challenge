04133902f243a6f88c0f7f568a67f62a44ba2f92
João Denis Rodrigues <joao.denisr@gmail.com>
2019-12-16 01:03:32 -0300

    handle error in update with blank url or category

--- navegg/sites/serializers.py
+++ navegg/sites/serializers.py
@@ -44,14 +44,18 @@ class SitesSerializer(serializers.ModelSerializer):
         model = Sites
         fields = ['id', 'name', 'url', 'category', 'active']
 
+    # Create the category or url for the sites if necessary
     def handle_reference(self, instance, reference_instance, data_list):
         for data in data_list:
             description = data['description']
+            # Verify if the category or url already exists, if not exists create a new
+            # if exist find the object
             if not reference_instance.objects.filter(description=description).exists():
                 reference_object = reference_instance.objects.create(description=description)
             else:
                 reference_object = reference_instance.objects.get(description=description)
 
+            # Add the searched/created object to the Site reference
             if reference_instance.__name__ == 'SiteCategory':
                 instance.category.add(reference_object)
             elif reference_instance.__name__ == 'SiteURL':
@@ -60,25 +64,33 @@ class SitesSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         site = Sites()
         site.name = validated_data['name']
+
+        # Save the site to be able to link the category and url to the site
         site.save()
         errors = { 'errors': []}
+
+        # Handle the url in the request
         if 'url' in validated_data:
             site.url.clear()
             request_urls = validated_data.pop('url')
+            # Check if the request contains at least one url
             if len(request_urls) == 0:
                 errors['errors'].append({'url': 'At least one URL is required'})
             else:
                 self.handle_reference(site, SiteURL, request_urls)
 
+        # Handle the category in the request
         if 'category' in validated_data:
             site.category.clear()
             request_categories = validated_data.pop('category')
+            # Check if the request contains at least one category
             if len(request_categories) == 0:
                 errors['errors'].append({'category': 'At least one Category is required'})
             else:
                 self.handle_reference(site, SiteCategory, request_categories)
 
         if len(errors['errors']) > 0:
+            # Delete the created site
             site.delete()
             raise serializers.ValidationError(errors)
         
@@ -89,15 +101,34 @@ class SitesSerializer(serializers.ModelSerializer):
         instance.name = validated_data['name']
         instance.save()
 
+        errors = { 'errors': []}
+
         if 'url' in validated_data:
-            instance.url.clear()
             request_urls = validated_data.pop('url')
-            self.handle_reference(instance, SiteURL, request_urls)
+
+            # Check if the request contains at least one url
+            if len(request_urls) == 0:
+                errors['errors'].append({'url': 'At least one URL is required'})
+            else:
+                # Remove all urls related to this site before adding the
+                # urls passed in the request
+                instance.url.clear()
+                self.handle_reference(instance, SiteURL, request_urls)
 
         if 'category' in validated_data:
-            instance.category.clear()
             request_categories = validated_data.pop('category')
-            self.handle_reference(instance, SiteCategory, request_categories)
+
+            # Check if the request contains at least one category
+            if len(request_categories) == 0:
+                errors['errors'].append({'category': 'At least one Category is required'})
+            else:
+                # Remove all categories related to this site before adding the
+                # categories passed in the request
+                instance.category.clear()
+                self.handle_reference(instance, SiteCategory, request_categories)
+
+        if len(errors['errors']) > 0:
+            raise serializers.ValidationError(errors)
 
         instance.save()
 
